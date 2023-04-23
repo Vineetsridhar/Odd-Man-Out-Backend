184c59064269d58165725a6a2377b97ef107b946
Vineet Sridhar <vineet@secureframe.com>
2023-04-23 11:07:49 -0400

    Connected to DB

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,30 @@
+import { Sequelize } from "sequelize";
 import express from "express";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const sequelize = new Sequelize(
+  "postgres",
+  process.env.USERNAME,
+  process.env.PASSWORD,
+  {
+    host: process.env.HOSTNAME,
+    dialect: "postgres",
+  }
+);
+
+const connect = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+};
+
+connect();
+
 const app = express();
 const port = 3000;
 
